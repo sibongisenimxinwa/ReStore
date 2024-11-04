0950ea012ff890f3dddb5856926d5ff7220ae137
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-11-04 10:36:07 +0200

    React Router

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './app/layout/style.css'	
-import App from './app/layout/App.tsx'
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './app/layout/style.css';
 
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
-
+import { RouterProvider } from 'react-router-dom';
+import { router } from './app/router/Routes';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
-  </StrictMode>
-)
+    <RouterProvider router={router} />
+  </StrictMode>,
+);
