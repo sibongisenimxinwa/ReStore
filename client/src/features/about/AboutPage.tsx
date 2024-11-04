377efcf4da2efd8aa9d61df1d4a1341ea29cef31
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-11-04 14:13:45 +0200

    Error handling

--- client/src/features/about/AboutPage.tsx
+++ client/src/features/about/AboutPage.tsx
@@ -1,7 +1,37 @@
-import { Typography } from "@mui/material";
+import { Container, Typography, ButtonGroup, Button, Alert, AlertTitle, List, ListItem, ListItemText } from "@mui/material";
+import { useState } from "react";
+import agent from "../../app/api/agent";
+
 
 export default function AboutPage() {
+    const [validationErrors, setValidationErrors] = useState<string[]>([]);
+    function getValidationError() {
+        agent.TestErrors.getValidationError()
+            .then(() => console.log('should not see this!'))
+            .catch(error => setValidationErrors(error));
+    }
     return (
-        <Typography variant="h2">About Page</Typography>
+        <Container>
+        <Typography gutterBottom variant={'h2'}>Errors for testing purposes</Typography>
+        <ButtonGroup fullWidth>
+            <Button onClick={() => agent.TestErrors.get500Error().catch(error => console.log(error))} variant={'contained'}>Test 500 error</Button>
+            <Button onClick={() => agent.TestErrors.get404Error().catch(error => console.log(error))} variant={'contained'}>Test 404 error</Button>
+            <Button onClick={() => agent.TestErrors.get400Error().catch(error => console.log(error))} variant={'contained'}>Test 400 error</Button>
+            <Button onClick={getValidationError} variant={'contained'}>Test 400 validation
+                error</Button>
+            <Button onClick={() => agent.TestErrors.get401Error().catch(error => console.log(error))} variant={'contained'}>Test 401 error</Button>
+        </ButtonGroup>
+        {validationErrors.length > 0 &&
+            <Alert severity="error">
+                <AlertTitle>Validation Errors</AlertTitle>
+                <List>
+                    {validationErrors.map(error => (
+                        <ListItem key={error}>
+                            <ListItemText>{error}</ListItemText>
+                        </ListItem>
+                    ))}
+                </List>
+            </Alert>}
+    </Container>
     );
 }
