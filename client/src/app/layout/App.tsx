c772ccc0f4dfc8aac1aa6e5805ec9ebf2b39af72
Sibongiseni Mxinwa <sibongiseni.mxinwa@gmail.com>
2024-11-13 12:47:33 +0200

    Adding the shopping cart feature

--- client/src/app/layout/App.tsx
+++ client/src/app/layout/App.tsx
@@ -5,12 +5,30 @@ import {
   createTheme,
 } from '@mui/material';
 import Header from './Header';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { useStoreContext } from '../context/StoreContext';
+import { getCookie } from '../util/util';
+import agent from '../api/agent';
+import LoadingComponent from './LoadingComponent';
 
 function App() {
+  const { setBasket } = useStoreContext();
+  const [loading, setLoading] = useState(true);
+  useEffect(() => {
+    const buyerId = getCookie('buyerId');
+    if (buyerId) {
+      agent.Basket.get()
+        .then((basket) => setBasket(basket))
+        .catch((error) => console.log(error))
+        .finally(() => setLoading(false));
+    } else {
+      setLoading(false);
+    }
+  }, [setBasket]);
+
   const [darkMode, setDarkMode] = useState(false);
   const palleteType = darkMode ? 'dark' : 'light';
   const theme = createTheme({
@@ -24,6 +42,9 @@ function App() {
   function handleThemeChange() {
     setDarkMode(!darkMode);
   }
+
+  if (loading) return <LoadingComponent message="Initiasing app..." />;
+
   return (
     <ThemeProvider theme={theme}>
       <ToastContainer position="bottom-right" hideProgressBar theme="colored" />
--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,16 +1,18 @@
-import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
 import './app/layout/style.css';
-
 import '@fontsource/roboto/300.css';
 import '@fontsource/roboto/400.css';
 import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { RouterProvider } from 'react-router-dom';
 import { router } from './app/router/Routes';
+import React from 'react';
+import { StoreProvider } from './app/context/StoreContext';
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <RouterProvider router={router} />
-  </StrictMode>,
+  <React.StrictMode>
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
+  </React.StrictMode>,
 );
